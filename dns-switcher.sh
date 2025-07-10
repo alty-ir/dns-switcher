eff488b0b56c472e0a20c8f37b47626d7889d73e
Mohammadhossein Fakhraei <linuxmaster@Mohammadhosseins-MacBook-Pro.local>
2025-07-10 22:21:23 +0330

    Remove Radar, and 403

--- dns-switcher.sh
+++ dns-switcher.sh
@@ -13,13 +13,11 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-providers=("Shecan" "Radar" "Electro" "Begzar" "403" "DNS Pro" "Google" "Cloudflare" "Reset to Default")
+providers=("Shecan" "Electro" "Begzar" "DNS Pro" "Google" "Cloudflare" "Reset to Default")
 declare -A dns_servers=(
     ["Shecan"]="178.22.122.100 185.51.200.2"
-    ["Radar"]="10.202.10.10 10.202.10.11"
     ["Electro"]="78.157.42.100 78.157.42.101"
     ["Begzar"]="185.55.226.26 185.55.226.25"
-    ["403"]="10.202.10.202 10.202.10.102"
     ["DNS Pro"]="87.107.110.109 87.107.110.110"
     ["Google"]="8.8.8.8 8.8.4.4"
     ["Cloudflare"]="1.1.1.1 1.0.0.1"
